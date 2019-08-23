4de74e758b4dc7c652e7f3a4f2a722b975eef8fc
Steven Bull <stevenwbulljr@gmail.com>
2019-08-22 20:35:56 -0500

    added functionality to Charges box in js

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ const store = {
 
 function watchForm () {
   $('#meal-details-form').submit( event => {
-    
-  })
+    event.preventDefault();
+    const mealPrice = $('.meal-price-entry').val();
+    const taxRate = $('.tax-rate-entry').val();
+    const tipPercentage = $('.tip-percentage-entry').val();
+    $('.meal-price-entry').val('');
+    $('.tax-rate-entry').val('');
+    $('.tip-percentage-entry').val('');
+    console.log(mealPrice);
+    doTheStaticMaths(mealPrice, taxRate, tipPercentage);
+  });
 }
+
+function doTheStaticMaths (mealPrice, taxRate, tipPercentage) {
+  const newTaxRate = taxRate / 100;
+  const newTipPercentage = tipPercentage / 100;
+  store.subTotal = (mealPrice * 1) + (mealPrice * newTaxRate);
+  store.tip = mealPrice * newTipPercentage;
+  store.total = store.subTotal + store.tip;
+  console.log(mealPrice * newTipPercentage);
+  handleCharges();
+}
+
+function handleCharges () {
+  console.log('handleCharges fn is running!');
+  $('.right-side-charges').html(`
+      <h2><i>Charges</i></h2>
+      <p>Subtotal $${store.subTotal}</p>
+      <p>Tip $${store.tip}</p>
+      <p>Total $${store.total}</p>
+    `);
+}
+
+function handleEarningsInfo () {
+
+}
+
+function renderPage() {
+  watchForm();
+}
+
+$(renderPage);
