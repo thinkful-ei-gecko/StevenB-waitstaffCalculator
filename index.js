9c5dc80faa303e292ffa0b67e5f4be5725f6d13a
Steven Bull <stevenwbulljr@gmail.com>
2019-08-22 20:59:11 -0500

    completed calculator functionality in javascript, added additional parameters to number inputs in html

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ const store = {
   avgTip: 0.00
 };
 
+function watchResetButton() {
+  $('.reset-button').on('click', event => {
+    event.preventDefault();
+    store.subTotal = 0;
+    store.tip = 0;
+    store.total = 0;
+    store.tipTotal = 0;
+    store.mealCount = 0;
+    store.avgTip = 0;
+    handleCharges();
+    handleEarningsInfo();
+  });
+}
+
+function watchCancelButton() {
+  $('.cancel-button').on('click', event => {
+    event.preventDefault();
+    $('.meal-price-entry').val('');
+    $('.tax-rate-entry').val('');
+    $('.tip-percentage-entry').val('');
+  });
+}
+
 function watchForm () {
   $('#meal-details-form').submit( event => {
     event.preventDefault();
@@ -19,22 +42,28 @@ function watchForm () {
     $('.tax-rate-entry').val('');
     $('.tip-percentage-entry').val('');
     console.log(mealPrice);
-    doTheStaticMaths(mealPrice, taxRate, tipPercentage);
+    doTheChargesMath(mealPrice, taxRate, tipPercentage);
   });
 }
 
-function doTheStaticMaths (mealPrice, taxRate, tipPercentage) {
+function doTheChargesMath (mealPrice, taxRate, tipPercentage) {
   const newTaxRate = taxRate / 100;
   const newTipPercentage = tipPercentage / 100;
   store.subTotal = (mealPrice * 1) + (mealPrice * newTaxRate);
   store.tip = mealPrice * newTipPercentage;
   store.total = store.subTotal + store.tip;
-  console.log(mealPrice * newTipPercentage);
   handleCharges();
+  doTheEarningsMath();
+}
+
+function doTheEarningsMath () {
+  store.tipTotal += store.tip;
+  store.mealCount++;
+  store.avgTip = (store.tipTotal / store.mealCount)
+  handleEarningsInfo();
 }
 
 function handleCharges () {
-  console.log('handleCharges fn is running!');
   $('.right-side-charges').html(`
       <h2><i>Charges</i></h2>
       <p>Subtotal $${store.subTotal}</p>
@@ -44,11 +73,18 @@ function handleCharges () {
 }
 
 function handleEarningsInfo () {
-
+  $('.right-side-earnings').html(`
+    <h2><i>Earnings Info</i></h2>
+    <p>Tip Total: $${store.tipTotal}</p>
+    <p>Meal Count: ${store.mealCount}</p>
+    <p>Average Tip per Meal: $${store.avgTip}</p>
+  `);
 }
 
 function renderPage() {
   watchForm();
+  watchResetButton();
+  watchCancelButton();
 }
 
 $(renderPage);
