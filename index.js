1cc753b129339a26a53c96cab1710de335a02b49
Steven Bull <stevenwbulljr@gmail.com>
2019-08-22 19:01:21 -0500

    added additional html classes, jQuery and javascript scripts and started on index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const store = {
+  subTotal: 0.00,
+  tip: 0.00,
+  total: 0.00,
+  tipTotal: 0.00,
+  mealCount: 0,
+  avgTip: 0.00
+};
+
+function watchForm () {
+  $('#meal-details-form').submit( event => {
+    
+  })
+}
